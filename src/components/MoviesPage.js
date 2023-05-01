8c75c4cdf0f5db14e5929ba1ced0688e98dcb0a1
florence wangechi <florencewangechim@gmail.com>
2023-05-01 14:24:08 +0300

    route dynamic

--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -1,11 +1,21 @@
 import React from "react";
-import { Route } from "react-router-dom";
-import MoviesList from "./MoviesList";
+import { Route,useRouteMatch } from "react-router-dom";
+import MovieList from "./MovieList";
+import MoviesShow from "./MoviesShow";
 
 function MoviesPage({ movies }) {
+  const match=useRouteMatch()
+  console.log(match)
   return (
     <div>
-      <MoviesList movies={movies} />
+      <MovieList movies={movies} />
+      <Route exact path={match.url}>
+        <h3>choose a movie from the list above. </h3>
+      </Route>
+      <Route path={`${match.url}/:movieid`}>
+      <MoviesShow movies={movies}/>
+      <h3>more information</h3>
+      </Route>
     </div>
   );
 }
